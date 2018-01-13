c3da0ed70b2a9d7d6090edf6bd9efc04e9f986be
Jeffrey Drakos <jeffreydrakos@Jeffreys-MacBook-Pro.local>
2018-01-12 23:38:09 -0500

    to string

--- src/android/Peripheral.java
+++ src/android/Peripheral.java
@@ -282,7 +282,7 @@ public class Peripheral extends BluetoothGattCallback {
             JSObject response = onSuccessCall(characteristic.getValue());
             
             LOG.d(TAG, "onCharacteristicChangedResponse2 " + response);
-            writeCallback.success(response);
+            writeCallback.success(response.toString());
         }
     }
 
