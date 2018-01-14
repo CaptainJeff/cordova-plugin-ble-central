27e6d580d89fdd297fd3d3d0bb152cb4987284cf
Jeffrey Drakos <jeffreydrakos@Jeffreys-MacBook-Pro.local>
2018-01-14 11:59:12 -0500

    log library

--- src/android/ActivityData.java
+++ src/android/ActivityData.java
@@ -1,6 +1,7 @@
 package com.megster.cordova.ble.central;
 
-import android.util.Log;
+import org.apache.cordova.LOG;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
